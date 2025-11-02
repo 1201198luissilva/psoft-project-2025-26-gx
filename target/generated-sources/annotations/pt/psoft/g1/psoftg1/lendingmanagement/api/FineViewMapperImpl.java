5a0b6daf63bca61c6a12f706f3e89c3636a39adb
Luis Silva <113892578+1201198luissilva@users.noreply.github.com>
2025-11-02 22:56:49 +0000

    ee

--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-02T22:04:59+0000",
+    date = "2025-11-02T22:56:22+0000",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251023-0518, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/services/LendingMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/services/LendingMapperImpl.java
@@ -6,7 +6,7 @@ import pt.psoft.g1.psoftg1.lendingmanagement.model.Lending;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-02T22:04:59+0000",
+    date = "2025-11-02T22:56:23+0000",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251023-0518, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
